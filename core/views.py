ba02e64d490a8deeb2f3209e77d34fefc24b9190
gvillablanca <g.villalobo.m@gmail.com>
2023-04-18 22:14:13 -0400

    tablas en administrador con datos desde la base de datos

--- core/views.py
+++ core/views.py
@@ -14,15 +14,27 @@ def usuario(request):
     return render(request, 'core/perfil-usuario.html')
 
 def administrador(request):
+    objectsU = Usuario.objects.all()
     objectsCot = Cotizacion.objects.all()
     objectsEvt = Evento.objects.all()
     objectsProv = Proveedor.objects.all()
     objectsSal = Salon.objects.all()
+    nombre_administrador = ''
+    correo = ''
+    cargo = ''
+    ubicacion = ''
+
+    for admin in objectsU:
+        if admin.tipo == "ADMINISTRADOR":
+            nombre_administrador = admin.nombre + ' ' + admin.apellido
+            correo = admin.correo
+            cargo = admin.cargo
+            ubicacion = admin.oficina    
 
     plantillaExterna = open("../LottusDream/core/templates/core/administrador.html")
     template = Template(plantillaExterna.read())
     plantillaExterna.close()
-    contexto=Context({"cotizaciones":objectsCot, "evento": objectsEvt, "salon": objectsSal, "proveedor": objectsProv})
+    contexto=Context({"cotizaciones":objectsCot, "evento": objectsEvt, "salon": objectsSal, "proveedor": objectsProv, "usuarios": objectsU, "nombre_administrador":nombre_administrador, "correo":correo, "cargo":cargo, "ubicacion":ubicacion})
     documento=template.render(contexto)
     return HttpResponse(documento)
 
